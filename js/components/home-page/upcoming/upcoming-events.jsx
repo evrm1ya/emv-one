bbc7e1bf9b39d95e1fb9410dc028a6ed66a747dd
evrm1ya <emvermilyea15@gmail.com>
2016-03-14 14:55:41 -0400

    add bubble styles

--- js/components/home-page/upcoming/upcoming-events.jsx
+++ js/components/home-page/upcoming/upcoming-events.jsx
@@ -5,8 +5,19 @@ import {EventBox} from './event-box';
 
 class UpcomingEvents extends React.Component {
   render() {
+    let bubbleColorOptions = ['#8652dc', '#333', '#e0e0e0'];
+    let opacityOptions = ['0.3', '0.5', '0.7', '0.9'];
     return <section className='home-upcoming-container'>
-      <BubbleOrnament />
+      <BubbleOrnament
+        containerClass='bubble-ornament-container'
+        bubbleClass='bubble'
+        artBoxClass='art-box'
+        minBubbleWidth={25}
+        totalBubbles={50}
+        containerWidth={300}
+        colorOptions={bubbleColorOptions}
+        opacityOptions={opacityOptions}
+      />
       <EventBox />
     </section>
   }
