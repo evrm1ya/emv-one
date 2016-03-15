6b397c33bd54a6e9de307beedf35c976a0cd91c3
evrm1ya <emvermilyea15@gmail.com>
2016-03-15 03:23:48 -0400

    image loading sketch

--- js/components/bass-page/bass-page.jsx
+++ js/components/bass-page/bass-page.jsx
@@ -3,8 +3,28 @@ import React, {Component, PropTypes} from 'react';
 import {Link} from 'react-router';
 
 class BassPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      yOffset: 0
+    };
+    this._updateYOffset = this._updateYOffset.bind(this);
+  }
+
+  _updateYOffset(event) {
+    console.log(window.pageYOffset);
+    this.setState({yOffset: window.pageYOffset});
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this._updateYOffset);
+  }
   render() {
-    return <div>
+    return <div className='bass-page'>
+      <img 
+        src='./public/images/jr-recital-small.jpg'
+        style={(this.state.yOffset > 20) ? {display: 'block'} : {display: 'none'}}
+      />
       Bass Page
     </div>
   }
