31d71f311cf6c847b82b67864c260bd1942e97a6
emvrm1ya <emvermilyea15@gmail.com>
2016-03-20 11:01:34 -0400

    remove bubble-ornament dir

--- js/components/bubble-ornament/src/randomizr/random-index.js
+++
@@ -1,6 +0,0 @@
-
-function getRandomIndex(intMax) {
-  return Math.floor(Math.random() * intMax);
-}
-
-export {getRandomIndex};
--- js/components/bubble-ornament/src/randomizr/random-nonzero-int.js
+++
@@ -1,6 +0,0 @@
-
-function getRandomNonZeroInt(intMax) {
-  return Math.floor(Math.random() * intMax) + 1;
-}
-
-export {getRandomNonZeroInt};
