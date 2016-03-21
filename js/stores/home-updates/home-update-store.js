9fb6a560d7feb91fb37102e70358400a826079fc
evrm1ya <emvermilyea15@gmail.com>
2016-03-21 11:52:18 -0400

    add update

--- js/stores/home-updates/home-update-store.js
+++ js/stores/home-updates/home-update-store.js
@@ -6,7 +6,8 @@
 var homeUpdateStore = [
   {id: 'POST_1', time: 'March 12 2016 - 7:51pm', text: 'Began building site'},
   {id: 'POST_2', time: 'March 13 2016 - 12:00am', text: 'Testing'},
-  {id: 'POST_3', time: 'March 20 2016 - 11:01pm', text: 'More testing'}
+  {id: 'POST_3', time: 'March 20 2016 - 11:01pm', text: 'More testing'},
+  {id: 'POST_4', time: 'March 21 2016 - 11:00am', text: 'Launch'}
 ];
 
 export {homeUpdateStore};
