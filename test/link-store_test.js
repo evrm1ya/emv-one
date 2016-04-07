9097e1600bb717abbbf087725452621f1f484a1e
Evan Vermilyea <emvermilyea15@gmail.com>
2016-04-07 01:18:18 -0400

    Delete link-store_test.js

--- test/link-store_test.js
+++
@@ -1,9 +0,0 @@
-
-import {expect} from 'chai';
-import {linkProps} from '../js/stores/nav/link-store';
-
-describe('link store', () => {
-  it('returns correct length', () => {
-    expect(linkProps.length).to.equal(5);
-  });
-});
